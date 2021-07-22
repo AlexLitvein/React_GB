5e64085b53dbd1d2dc0ff58e29af1bc2923ac652
Литвиновский Алексей Борисович <LITVINOVSKIY-AB@sibir.FSK-EES.LOCAL>
2021-07-22 09:27:14 +0700

    remove indents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Grid, List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
+import { Box, List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import orange from '@material-ui/core/colors/orange';
@@ -47,42 +47,43 @@ function App() {
   return (
     <ThemeProvider theme={myTheme}>
       <CssBaseline />
-      <div className="App">
-        <Grid container spacing={1}>
-          <Grid className="flxCont" item xs={4}>
-            <div className="flxItm brd">
-              <header className="App-header ">
-                <img src={logo} className="App-logo" alt="logo" />
-                <Tittle text={"Привет!"} />
-              </header>
-              <List component="nav" aria-label="main mailbox folders">
-                {chatList.map((itm) => (
-                  <ListItem key={itm.id} button>
-                    <ListItemIcon>
-                      <Chat />
-                    </ListItemIcon>
-                    <ListItemText primary={itm.name} />
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-          </Grid>
-          <Grid className="flxCont" container spacing={1} item xs={8}>
-            <Grid className="flxItm" item xs={12}>
-              <div className="msg-list brd">
-                {messageList.map((msg, idx) => (
-                  <Message key={idx} msg={msg} />
-                ))}
-              </div>
-            </Grid>
-            <Grid item xs={12}>
-              <SendForm addMessage={addMessage} />
-            </Grid>
-          </Grid>
-        </Grid>
+      <div className="App ">
+        <Box className="flxCont flx-col brd App__sidebar">
+          <header className="App-header ">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Tittle text={"Привет!"} />
+          </header>
+          <List component="nav" aria-label="main mailbox folders">
+            {chatList.map((itm) => (
+              <ListItem key={itm.id} button>
+                <ListItemIcon>
+                  <Chat />
+                </ListItemIcon>
+                <ListItemText primary={itm.name} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box className="flxCont flx-col brd App__main">
+          <div className="msg-list flx-grw brd">
+            {messageList.map((msg, idx) => (
+              <Message key={idx} msg={msg} />
+            ))}
+          </div>
+          <SendForm addMessage={addMessage} />
+        </Box>
       </div >
     </ThemeProvider>
   );
+
+  // return (
+
+  //     <div className="App ">
+
+  //         <SendForm addMessage={addMessage} />
+
+  //     </div >
+  // );
 }
 
 export default App;
