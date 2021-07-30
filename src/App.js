6e97c4a9208744adcf267a9df1f45d03683f48f4
Литвиновский Алексей Борисович <LITVINOVSKIY-AB@sibir.FSK-EES.LOCAL>
2021-07-30 08:52:03 +0700

    less-6

--- src/App.js
+++ src/App.js
@@ -1,49 +1,18 @@
 import { Box } from '@material-ui/core';
-import logo from "./logo.svg";
 import "./App.css";
-import Tittle from "./Tittle";
-import MessageList from "./MessageList";
-import Profile from "./profile";
-import {
-  Switch,
-  Link,
-  Route,
-} from "react-router-dom";
 import ChatList from "./chatList";
+import Menu from "./menu";
+import ChatRouter from './router';
 
 function App() {
   return (
     <div className="App ">
       <Box className="flxCont flx-col brd App__sidebar">
-        <header className="App-header ">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Tittle text={"Привет!"} />
-          <ul>
-            <li>
-              <Link to="/profile">profile</Link>
-            </li>
-            <li>
-              <Link to="/chats/bb">chats</Link>
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </header>
+        <Menu />
         <ChatList />
       </Box>
       <Box className="flxCont flx-col brd App__main">
-        <Switch>
-          <Route path="/profile"><Profile />
-          </Route>
-          <Route path="/chats/:chatId" children={<MessageList />}>
-          </Route>
-          <Route path="/"><h1>Home</h1>
-          </Route>
-          <Route>
-            <h3>Page not found</h3>
-          </Route>
-        </Switch>
+        <ChatRouter />
       </Box>
     </div >
   );
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+import MessageList from "./MessageList";
+import Profile from "./profile";
+
+const ChatRouter = () => {
+    return (
+        <Switch>
+            <Route path="/profile"><Profile />
+            </Route>
+            <Route path="/chats/:chatId" children={<MessageList />}>
+            </Route>
+            <Route path="/"><h1>Home</h1>
+            </Route>
+            <Route>
+                <h3>Page not found</h3>
+            </Route>
+        </Switch>
+    );
+}
+
+export default ChatRouter;
